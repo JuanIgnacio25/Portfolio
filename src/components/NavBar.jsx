54bd2684643f66ce2cae30a0b6747c6e8308b4f2
juan Ignacio Colli <Juan Ignacio Colli>
2023-09-07 17:04:59 -0300

    NavBar y estilos generales

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,12 +1,60 @@
-import React from 'react'
+import React from "react";
 
-
-function NavBar() {
+const NavBar = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
-  )
-}
+    <nav className="navbar navbar-expand-md navbar-light">
+      <div className="container-fluid nav-container">
+        <button
+          className="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span className="navbar-toggler-icon"></span>
+        </button>
 
-export default NavBar
+        <div className="collapse navbar-collapse" id="navbarSupportedContent">
+          <a className="navbar-brand" href="#">
+            <img
+              src="/LogoPortfolio.png"
+              width="50px"
+              alt="Logo J-ignacio de la barra de navegacion"
+            />
+          </a>
+          <ul className="navbar-nav">
+            <li className="nav-item">
+              <a className="nav-link" href="#">
+                Inicio
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#sobre-mi" aria-current="page">
+                Sobre Mi
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#tecnologias">
+                Tecnologias
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#proyectos">
+                Proyectos
+              </a>
+            </li>
+            <li className="nav-item">
+              <a className="nav-link" href="#contacto">
+                Contacto
+              </a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
