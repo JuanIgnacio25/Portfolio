115a5247f371471565e0c38e2b0aabfdb7f751e8
Juan Ignacio Colli <Juan Ignacio Colli>
2023-11-13 14:53:23 -0300

    animaciones de scroll con scrollReveal agregado

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,32 @@
-import NavBar from "./components/NavBar"
-import SideBar from "./components/SideBar"
-import Hero from "./components/Hero"
-import AboutMe from "./components/AboutMe"
-import Projects from "./components/Projects"
-import Technologies from "./components/Technologies"
-import Contact from "./components/Contact"
-import Footer from "./components/Footer"
-import './App.css'
+import NavBar from "./components/NavBar";
+import SideBar from "./components/SideBar";
+import Hero from "./components/Hero";
+import AboutMe from "./components/AboutMe";
+import Projects from "./components/Projects";
+import Technologies from "./components/Technologies";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import { useEffect } from "react";
+import { loadTransitions } from "./utils/scrollReveal";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    loadTransitions();
+  }, [])
   
-
   return (
-    <div className='App'>
-      <NavBar/>
-      <SideBar/>
-      <Hero/>
-      <AboutMe/>
-      <Projects/>
-      <Technologies/>
-      <Contact/>
-      <Footer/>
+    <div className="App">
+      <NavBar />
+      <SideBar />
+      <Hero />
+      <AboutMe />
+      <Projects />
+      <Technologies />
+      <Contact />
+      <Footer />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/utils/scrollReveal.js
@@ -0,0 +1,22 @@
+import ScrollReveal from "scrollreveal";
+
+const sr = ScrollReveal({
+  distance: "65px",
+  duration: 2600,
+  delay: 450,
+  reset: false,
+});
+
+export const loadTransitions = () => {
+    sr.reveal('.img-container', {delay:200, origin:'top'})
+    sr.reveal('.hero h1', {delay:200, origin:'bottom'})
+    sr.reveal('.hero h3', {delay:200, origin:'bottom'})
+    sr.reveal('.about-me-title', {delay:250, origin:'left'})
+    sr.reveal('.about-me-description', {delay:250, origin:'right'})
+    sr.reveal('.project-title', {delay:250, origin:'top'})
+    sr.reveal('.projects-container', {delay:250, origin:'bottom'})
+    sr.reveal('.technologies-container h1', {delay:250, origin:'top'})
+    sr.reveal('.technologie', {delay:250, origin:'bottom'})
+    sr.reveal('.contact-container', {delay:450, origin:'top'})
+    
+};
