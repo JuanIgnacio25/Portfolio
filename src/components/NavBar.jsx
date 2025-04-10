67441b05f0ef4e891ece376fd55ff586f8f47e10
Juan Ignacio Colli <juanignaciocolli28@gmail.com>
2025-04-10 10:24:38 -0300

    Modified, responsive styles

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,62 +1,41 @@
+import { useState } from "react";
+import { HiOutlineViewList } from "react-icons/hi";
 
 const NavBar = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-  const handleSelectSection = (e,id) => {
+  const handleSelectSection = (e, id) => {
+    e.preventDefault();
     const target = document.querySelector(id);
     if (target) {
       target.scrollIntoView({ behavior: "smooth" });
+      setIsOpen(false); // cerrar el menú al hacer click
     }
-  }
+  };
 
   return (
-    <nav className="navbar navbar-expand-md navbar-dark fixed-top">
-      <div className="container-fluid nav-container">
+    <div className="custom-navbar">
+      <div className="custom-navbar-wrapper">
+        <a className="custom-navbar-logo" href="#">
+          <img src="/logo.svg" alt="Logo" width="50" />
+        </a>
+
         <button
-          className="navbar-toggler"
-          type="button"
-          data-bs-toggle="collapse"
-          data-bs-target="#navbarSupportedContent"
-          aria-controls="navbarSupportedContent"
-          aria-expanded="false"
-          aria-label="Toggle navigation"
+          className="custom-navbar-toggler"
+          onClick={() => setIsOpen(!isOpen)}
+          aria-label="Toggle menu"
         >
-          <span className="navbar-toggler-icon"></span>
+          <HiOutlineViewList size={28} color="white" />
         </button>
 
-        <a className="navbar-brand" href="#">
-            <img
-              src="/logo.svg"
-              width="50px"
-              alt="Logo del porfolio"
-            />
-          </a>
-
-        <div className="collapse navbar-collapse" id="navbarSupportedContent">
-          <ul className="navbar-nav ms-auto">
-            <li className="nav-item">
-              <a className="nav-link" href="#sobre-mi" aria-current="page" onClick={(e) => handleSelectSection(e, "#sobre-mi")} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                Sobre Mi
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#proyectos" onClick={(e) => handleSelectSection(e, "#proyectos")} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                Proyectos
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#tecnologias" onClick={(e) => handleSelectSection(e, "#tecnologias")} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                Tecnologias
-              </a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#contacto" onClick={(e) => handleSelectSection(e, "#contacto")} data-bs-toggle="collapse" data-bs-target=".navbar-collapse.show">
-                Contacto
-              </a>
-            </li>
-          </ul>
-        </div>
+        <nav className={`custom-navbar-menu ${isOpen ? "show" : ""}`}>
+          <a onClick={(e) => handleSelectSection(e, "#sobre-mi")}>Sobre mí</a>
+          <a onClick={(e) => handleSelectSection(e, "#proyectos")}>Proyectos</a>
+          <a onClick={(e) => handleSelectSection(e, "#tecnologias")}>Tecnologías</a>
+          <a onClick={(e) => handleSelectSection(e, "#contacto")}>Contacto</a>
+        </nav>
       </div>
-    </nav>
+    </div>
   );
 };
 
